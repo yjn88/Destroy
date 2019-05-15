af307e6c01a96b09603ca9837a9bed022efa3cb8
GreatDestroyerCharlie <greatdestroyercharlie@gmail.com>
2019-05-15 22:03:08 +0300

    Creat Singleton.cs

---
+++ Destroy/Destroy/Base/Singleton.cs
@@ -0,0 +1,25 @@
+﻿namespace Destroy
+{
+    /// <summary>
+    /// 继承该类获得单例 (没有为多线程考虑)
+    /// </summary>
+    public class Singleton<T> where T : new()
+    {
+        private static T instance;
+
+        /// <summary>
+        /// 单例
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+                return instance;
+            }
+        }
+    }
+}
